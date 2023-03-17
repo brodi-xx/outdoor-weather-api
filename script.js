0fa8f6ee9f1bddcd85de35a17d00b6d3df90b40b
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-03-16 21:47:39 -0500

    added in google api key and fetch code placeholder

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const categories = document.querySelectorAll('.category'); categories.forEach(category => { new Chosen(category, {disable_search_threshold: 10}); }); 
+// API Key placeholder - 
+// Google API Key: AIzaSyCFF4YG_cQn0Sj5LZ8IqXjx4aemkvj8pZ4
+// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFF4YG_cQn0Sj5LZ8IqXjx4aemkvj8pZ4&libraries=places">
+
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data here
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors here
+    console.error(error);
+  });
