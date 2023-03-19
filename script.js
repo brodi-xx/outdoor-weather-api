b88fa66a1221f80fb5e7bf4a6fd2221a72cdd157
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-03-19 10:17:27 -0500

    re-added my original code, it did not stay after my commit and push

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 const categories = document.querySelectorAll('.category'); categories.forEach(category => { new Chosen(category, {disable_search_threshold: 10}); }); 
 console.log (categories)
+
+// API Key placeholder - 
+// Google API Key: AIzaSyCFF4YG_cQn0Sj5LZ8IqXjx4aemkvj8pZ4
+// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFF4YG_cQn0Sj5LZ8IqXjx4aemkvj8pZ4&libraries=places">
+
+function initMap() {
+  // Map location options 
+  var options = {
+    zoom:7,
+    center:{lat:19.4326,lng:-99.1332}
+  }
+
+  // New Map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+}
+
+
+/*fetch('https://maps.googleapis.com/maps/api/js?key=AIzaSyCFF4YG_cQn0Sj5LZ8IqXjx4aemkvj8pZ4&libraries=places')
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data here
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors here
+    console.error(error);
+  }); */
