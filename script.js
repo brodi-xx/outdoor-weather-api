8a03009cd6ed6ef1b7a903e10eeae514c5ed0496
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-03-19 18:21:50 -0400

    Add querySelector for each element, creat eventlistener for search button

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const categories = document.querySelectorAll('.category'); categories.forEach(category => { new Chosen(category, {disable_search_threshold: 10}); }); 
-console.log (categories)
+const categories = document.querySelector('.category');
+// categories.forEach(category => {
+//      new Chosen(category, {disable_search_threshold: 10}); 
+//     }); 
+const keyword = document.querySelector('#keyword');
+const location = document.querySelector('#location');
+const search = document.querySelector("#search");
+
+search.addEventListener("click", function() {
+    localStorage.setItem("keyword", keyword);
+    localStorage.setItem("location", location);
+    localStorage.setItem("category", category);
+})
