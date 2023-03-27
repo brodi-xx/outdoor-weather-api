1e84c5e8070b12960151371c74a2c1371c41483c
LeybaAir <123996216+LeybaAir@users.noreply.github.com>
2023-03-27 16:03:09 -0500

    newest updates for map

--- script.js
+++ script.js
@@ -22,10 +22,11 @@ searchBtn.addEventListener('click', () => {
   let locationData = document.createElement("div");
   locationData.innerHTML = localStorage.getItem("location");
   searchHistory.appendChild(locationData);
-  
+
   // Get the user's input
   console.log('loc valu', locationName)
   const endpointUrl = `${baseUrl}${locationName}&units=imperial&appid=${apiKey}`;
+  // getCoords(locationName);
 
   // keywords = document.getElementById('keywords').value;
   // const category = document.querySelector('.category').value;
@@ -34,14 +35,15 @@ searchBtn.addEventListener('click', () => {
   fetch(endpointUrl)
     .then(response => response.json())
     .then(data => {
-      console.log ("data",data);
+      console.log("data", data);
+      initMap(data.coord.lat, data.coord.lon);
       // Process the API response data
       const weatherDescription = data.weather[0].description;
       const temperature = data.main.temp;
       iconcode = data.weather[0].icon;
-      console.log ("icon",iconcode)
+      console.log("icon", iconcode)
       const iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-      console.log ("iconUrl",iconurl)
+      console.log("iconUrl", iconurl)
       // const locationNameName = data.name;
       // Display the weather information on the page
       const weatherInfo = `The weather in ${locationName} is currently ${weatherDescription} with a temperature of ${temperature} F.`;
@@ -68,7 +70,7 @@ searchBtn.addEventListener('click', () => {
       const weather = data.weather[0].main.toLowerCase();
       // Check if weather is suitable for selected activity category
       if ((category === 'indoor' && weather !== 'rain' && weather !== 'snow') ||
-          (category === 'outdoor' && weather === 'clear')) {
+        (category === 'outdoor' && weather === 'clear')) {
         console.log(`Weather is suitable for ${category} activity`);
       } else {
         console.log(`Weather is not suitable for ${category} activity`);
@@ -77,33 +79,94 @@ searchBtn.addEventListener('click', () => {
     .catch(error => console.log(error));
 });
 
-function initMap() {
-  // Map locationName options
-  var options = {
-    zoom:7,
-    center:{lat:19.4326,lng:-99.1332}
-  }
-  // New Map
-  var map = new google.maps.Map(document.getElementById('map'), options);
+// function getCoords(city) {
+//   var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data.coord);
+//       // initMap(data.coord.lat, data.coord.lon);
+//     })
+// }
 
-// Create the search box and link it to the UI element.
-const input = document.getElementById("pac-input");
-const searchBox = new google.maps.places.SearchBox(input);
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-// Bias the SearchBox results towards current map's viewport.
-map.addListener("bounds_changed", () => {
-  searchBox.setBounds(map.getBounds());
-});
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-// Bias the SearchBox results towards current map's viewport.
-map.addListener("bounds_changed", () => {
-  searchBox.setBounds(map.getBounds());
-});
+// function initMap(lat, lng) {
+//   // Map locationName options
+//   var options = {
+//     zoom: 7,
+//     center: { lat: lat, lng: lng }
+//   }
+//   // New Map
+//   var map = new google.maps.Map(document.getElementById('map'), options);
+
+//   const request = {
+//     query: "food",
+//     type: ['restaurant']
+//   };
+
+//   // Create the search box and link it to the UI element.
+//   const input = document.getElementById("pac-input");
+//   const searchBox = new google.maps.places.SearchBox(input);
+//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//   // Bias the SearchBox results towards current map's viewport.
+//   map.addListener("bounds_changed", () => {
+//     searchBox.setBounds(map.getBounds());
+//   });
+//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//   // Bias the SearchBox results towards current map's viewport.
+//   map.addListener("bounds_changed", () => {
+//     searchBox.setBounds(map.getBounds());
+//   });
+// }
+
+function initMap(lat, lon) {
+  const latitude = lat || 10
+  const longitude = lon || 10
+  const sydney = new google.maps.LatLng(latitude, longitude);
+
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: sydney,
+    zoom: 15,
+  });
+
+  const request = {
+    query: "mall",
+    // type: ['restaurant'],
+    fields: ["name"],
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+        console.log(results[i])
+      }
+
+      // map.setCenter(results[0].geometry.location);
+    }
+  });
 }
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
+
+window.initMap = initMap;
 
 closeButton.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
 
-initMap();
+// initMap();
